c41b5845b5213c93629f42fb958966fb8f0297c1
Jose Sa <jose-a-sa@users.noreply.github.com>
2023-02-17 03:27:46 +0000

    remove extra print statements

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,22 @@
-#include <boost/tokenizer.hpp>
-#include <boost/range/algorithm.hpp>
 #include <boost/lexical_cast.hpp>
-#include <iterator>
 #include <iostream>
 #include <stack>
-
 #include <vector>
+
 #include "Point.hpp"
 #include "TriangulationFlipGraph.hpp"
 
-template<typename T>
-void parseArgument(std::vector<Point<T, 2>>& res, const std::string& sv)
+template <typename T>
+void parseArgument(std::vector<Point<T, 2>> &res, const std::string &sv)
 {
-	auto numParser = [&](const std::string& s) -> T
+	auto numParser = [&](const std::string &s) -> T
 	{
 		T res;
 		try
 		{
 			res = boost::lexical_cast<T>(s);
 		}
-		catch (const boost::bad_lexical_cast& e)
+		catch (const boost::bad_lexical_cast &e)
 		{
 			std::cerr << e.what() << '\n';
 			std::exit(1);
@@ -27,11 +24,11 @@ void parseArgument(std::vector<Point<T, 2>>& res, const std::string& sv)
 		return res;
 	};
 
-	auto stackToPointVector = [&numParser, &res](std::stack<std::string>& st) -> void
+	auto stackToPointVector = [&numParser, &res](std::stack<std::string> &st) -> void
 	{
 		if (st.size() != 2)
 		{
-			std::cerr << "Dimension of point is not 2." << std::endl;
+			std::cerr << "ERROR: Dimension of point is not 2." << std::endl;
 			std::exit(1);
 		}
 		Point<T, 2> pt;
@@ -45,15 +42,15 @@ void parseArgument(std::vector<Point<T, 2>>& res, const std::string& sv)
 	std::string curr;
 	std::stack<std::string> st;
 
-	for (const char& c : sv)
+	for (const char &c : sv)
 	{
 		if (c == '(' || isspace(c))
 			continue;
 		else if (c == ')' || c == ',')
 		{
-			std::cout << curr << std::endl;
 			st.push(curr);
-			if (c == ')') stackToPointVector(st);
+			if (c == ')')
+				stackToPointVector(st);
 			curr.clear();
 		}
 		else
@@ -61,11 +58,11 @@ void parseArgument(std::vector<Point<T, 2>>& res, const std::string& sv)
 	}
 }
 
-int main(int argc, const char* argv[])
+int main(int argc, const char *argv[])
 {
 	if (argc != 2)
 	{
-		std::cerr << "Expecting 1 argument as a string of points." << std::endl;
+		std::cerr << "ERROR: Expecting 1 argument as a string of points." << std::endl;
 		std::exit(1);
 	}
 
@@ -76,9 +73,9 @@ int main(int argc, const char* argv[])
 	TriangulationFlipGraph gr(std::move(coords));
 	gr.generateGraph();
 
-	for (const auto& mesh : gr.vertices())
+	for (const auto &mesh : gr.vertices())
 		std::cout << mesh.wkt() << std::endl;
-	for (const auto& eg : gr.edges())
+	for (const auto &eg : gr.edges())
 		std::cout << eg << " ";
 	std::cout << std::endl;
 
