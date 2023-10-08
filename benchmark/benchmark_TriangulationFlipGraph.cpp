f484d29bc322172afb4fa3a7d41d560e1d053dc7
Jose Sa <jose-a-sa@users.noreply.github.com>
2023-10-08 19:06:51 +0100

    added GBenchmark
    Signed-off-by: Jose Sa <jose-a-sa@users.noreply.github.com>

--- benchmark/benchmark_TriangulationFlipGraph.cpp
+++ benchmark/benchmark_TriangulationFlipGraph.cpp
@@ -2,9 +2,15 @@
 
 #include "MeshTriangulation.hpp"
 #include "TriangulationFlipGraph.hpp"
-#include "../tests/Factory.hpp"
+#include "Factory.hpp"
 #include <memory>
 
+double RollingMean(double currentMean, double entry, std::size_t iteration)
+{
+    double n;
+    n = static_cast<double>(iteration);
+    return (currentMean * n + entry) / (n + 1);
+}
 
 static void BM_TriangulationFlipGraph_generateGraph(benchmark::State& state)
 {
@@ -12,16 +18,25 @@ static void BM_TriangulationFlipGraph_generateGraph(benchmark::State& state)
     {
         state.PauseTiming();
         auto coords = PointFactory::randomSample(state.range(0));
+
         state.ResumeTiming();
         TriangulationFlipGraph mesh(*coords);
         mesh.generateGraph();
+
+        state.PauseTiming();
+        state.counters["Edges"] += static_cast<double>(mesh.edges().size());
+        state.counters["Nodes"] += static_cast<double>(mesh.vertices().size());
     }
+
     state.SetComplexityN(state.range(0));
 }
+
 BENCHMARK(BM_TriangulationFlipGraph_generateGraph)
-    ->Iterations(40)
     ->DenseRange(4, 13, 1)
-    ->Complexity([](benchmark::IterationCount n) -> double
-                 { return std::exp(n); });
+    ->Iterations(1)
+    ->Repetitions(100)
+    ->Complexity([](benchmark::IterationCount N) -> double
+                 { return std::exp(1.2 * N); })
+    ->ReportAggregatesOnly();
 
 BENCHMARK_MAIN();
